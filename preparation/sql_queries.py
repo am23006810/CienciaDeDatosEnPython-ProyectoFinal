05a052da9e33bb9a8cea109c77285623d14ef4c8
andreamz05 <74270573+andreamz05@users.noreply.github.com>
2023-04-07 18:13:26 -0600

    insert

--- preparation/sql_queries.py
+++ preparation/sql_queries.py
@@ -1,12 +1,12 @@
 DDL_QUERY =  '''
 CREATE TABLE IF NOT EXISTS rose_wine(
-    name VARCHAR(70),
-    country VARCHAR(20),
-    region VARCHAR(40),
-    winery VARCHAR(50),
-    rating float,
-    number_of_ratings int,
-    price float,
-    year VARCHAR(10)
+    Name VARCHAR(70),
+    Country VARCHAR(20),
+    Region VARCHAR(40),
+    Winery VARCHAR(70),
+    Rating float,
+    NumberOfRatings int,
+    Price float,
+    Year VARCHAR(10)
 );
 '''
